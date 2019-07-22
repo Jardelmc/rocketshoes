d7b6746119ba14e2b61afada8648abd2a3e87f4c
Jardelmc <jardel.casteluber@gmail.com>
2019-07-21 23:02:11 -0300

    chore - Increment styles from shoes catalog

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,80 @@
 import React from 'react';
-
-// import { Container } from './styles';
+import { MdAddShoppingCart } from 'react-icons/md';
+import { ProductList } from './styles';
 
 export default function Home() {
-  return <div />;
+  return (
+    <ProductList>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-kappa-impact-masculino/04/D24-1738-304/D24-1738-304_detalhe2.jpg?resize=326:*"
+          alt="Tênis"
+        />
+        <strong>Tênis cool</strong>
+        <span>R$ 130,00</span>
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 3
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>{' '}
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-kappa-impact-masculino/04/D24-1738-304/D24-1738-304_detalhe2.jpg?resize=326:*"
+          alt="Tênis"
+        />
+        <strong>Tênis cool</strong>
+        <span>R$ 130,00</span>
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 3
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>{' '}
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-kappa-impact-masculino/04/D24-1738-304/D24-1738-304_detalhe2.jpg?resize=326:*"
+          alt="Tênis"
+        />
+        <strong>Tênis cool</strong>
+        <span>R$ 130,00</span>
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 3
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>{' '}
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-kappa-impact-masculino/04/D24-1738-304/D24-1738-304_detalhe2.jpg?resize=326:*"
+          alt="Tênis"
+        />
+        <strong>Tênis cool</strong>
+        <span>R$ 130,00</span>
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 3
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>{' '}
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-kappa-impact-masculino/04/D24-1738-304/D24-1738-304_detalhe2.jpg?resize=326:*"
+          alt="Tênis"
+        />
+        <strong>Tênis cool</strong>
+        <span>R$ 130,00</span>
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 3
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>{' '}
+      </li>
+    </ProductList>
+  );
 }
